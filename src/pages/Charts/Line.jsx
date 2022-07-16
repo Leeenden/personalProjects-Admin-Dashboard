5aee97935112a40904b664491bbff562f0b3a47f
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-07-16 13:08:28 +0100

    wokring area chart

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -4,8 +4,8 @@ import { Header, LineChart } from "../../components";
 const Line = () => {
   return (
     <div className="m-4 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Header category="Chart" title="inflation Rate"/>
-      <div colassName="w-full">
+      <Header category="Line Chart" title="Inflation Rate over Time"/>
+      <div className="w-full">
         <LineChart />
       </div>
     </div>
